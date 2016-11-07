4ec010e88e2a3caf71134d195b45e09ce9cbfc9b
Tristan Miller <psychonaut@nothingisreal.com>
2016-11-07 22:25:40 +0100

    Import v1.1

--- src/uk/ac/man/cs/choif/extend/sort/Comparator.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.man.cs.choif.extend.sort;
-
-/**
- * Comparator interface
- * Creation date: (07/29/99 22:22:39)
- * @author: Freddy Choi
- */
-public interface Comparator extends sun.misc.Compare {
-}
